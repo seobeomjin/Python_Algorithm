3c3f529a867076e79a593c098687d9ccbc4d4fea
seobeomjin <chon0412@khu.ac.kr>
2021-01-30 20:36:51 +0900

    int to str

--- LeetCode/ReverseInteger.py
+++ LeetCode/ReverseInteger.py
@@ -4,10 +4,34 @@
     만약 reversed value의 범위가 [-2^31, 2^31-1] 을 넘으면 return 0 하라. 
 
 - solved in 25min (official Easy / for me: ★☆☆☆☆))
-    36 ms / 14.4 MB
-    분명 더 효율적인 방법이 있을텐데.. 
+    28 ms / 14.2 MB
+    faster than 89.27% of Python3 online submissions
+
+- 깨달은 점 
+    > str 을 list로 wapping하면 index별로 쪼개서 들어감 
+    > list의 거꾸로 꺼내는 것은 list[::-1] 임. 
+    > abs를 활용하면 연산에서 더 빠름 
 
 """
+# My developed solution 
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x < 0 : 
+            rev_x = str(x)[1:][::-1]
+        else : 
+            rev_x = str(x)[::-1]
+            
+        if x < 0 :
+            rev_x = int("-"+rev_x)
+            if (-2)**31 <= rev_x <= 2**31-1 : return rev_x
+            else : return 0
+            
+        else : 
+            rev_x = int(rev_x)
+            if (-2)**31 <= rev_x <= 2**31-1 : return rev_x
+            else : return 0
+
+
 
 # My solution
 class Solution:
@@ -32,7 +56,9 @@ class Solution:
         else : 
             return 0
 
-# effective solution via another coder 
+
+
+# effective solution by another coder 
 class Solution:
     def reverse(self, x: int) -> int:
         numstr=str(abs(x))
